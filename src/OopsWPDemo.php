e8230318b0d82386022adfbb840a93299d9ccd56
Jeremy Ward <jeremy@jmichaelward.com>
2020-01-31 10:36:01 -0600

    Create plugin bootstrap file and plugin class.

---
+++ src/OopsWPDemo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-01-31
+ * @package WebDevStudios\OopsWPDemo
+ */
+
+namespace WebDevStudios\OopsWPDemo;
+
+/**
+ * Class OopsWPDemo
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-01-31
+ * @package WebDevStudios\OopsWPDemo
+ */
+class OopsWPDemo {
+
+}
