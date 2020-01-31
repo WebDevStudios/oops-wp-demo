79a5b3e70566c1346fd92a0fbf55e178dc7655b4
Jeremy Ward <jeremy@jmichaelward.com>
2020-01-31 11:20:32 -0600

    Add slug and labels to post type.

--- src/Content/PostType/Game.php
+++ src/Content/PostType/Game.php
@@ -20,6 +20,14 @@ use WebDevStudios\OopsWP\Structure\Content\PostType;
  */
 class Game extends PostType {
 	/**
+	 * The post type slug.
+	 *
+	 * @var string
+	 * @since 2020-01-30
+	 */
+	protected $slug = 'wds-game';
+
+	/**
 	 * Get the labels for this post type.
 	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
@@ -27,6 +35,21 @@ class Game extends PostType {
 	 * @return array
 	 */
 	protected function get_labels() : array {
-		return [];
+		return [
+			'name'               => _x( 'Games', 'post type general name', 'oops-wp-demo' ),
+			'singular_name'      => _x( 'Game', 'post type singular name', 'oops-wp-demo' ),
+			'menu_name'          => _x( 'Games', 'admin menu', 'oops-wp-demo' ),
+			'name_admin_bar'     => _x( 'Game', 'add new on admin bar', 'oops-wp-demo' ),
+			'add_new'            => _x( 'Add New', 'game', 'oops-wp-demo' ),
+			'add_new_item'       => __( 'Add New Game', 'oops-wp-demo' ),
+			'new_item'           => __( 'New Game', 'oops-wp-demo' ),
+			'edit_item'          => __( 'Edit Game', 'oops-wp-demo' ),
+			'view_item'          => __( 'View Game', 'oops-wp-demo' ),
+			'all_items'          => __( 'All Games', 'oops-wp-demo' ),
+			'search_items'       => __( 'Search Games', 'oops-wp-demo' ),
+			'parent_item_colon'  => __( 'Parent Games:', 'oops-wp-demo' ),
+			'not_found'          => __( 'No games found.', 'oops-wp-demo' ),
+			'not_found_in_trash' => __( 'No games found in Trash.', 'oops-wp-demo' ),
+		];
 	}
 }
