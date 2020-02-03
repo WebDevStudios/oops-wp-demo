8820c92fd67143af05b6651fdb17551444f36540
Jeremy Ward <jeremy@jmichaelward.com>
2020-02-03 14:02:12 -0600

    Set up super basic npm scripts.

---
+++ src/BlockEditor/Block/Game.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-02-03
+ * @package WebDevStudios\OopsWPDemo\BlockEditor\Block
+ */
+
+namespace WebDevStudios\OopsWPDemo\BlockEditor\Block;
+
+/**
+ * Class Game
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-02-03
+ * @package WebDevStudios\OopsWPDemo\BlockEditor\Block
+ */
+class Game {
+
+}
---
+++ src/BlockEditor/BlockRegistrar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-02-03
+ * @package WebDevStudios\OopsWPDemo\BlockEditor
+ */
+
+namespace WebDevStudios\OopsWPDemo\BlockEditor;
+
+/**
+ * Class BlockRegistrar
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-02-03
+ * @package WebDevStudios\OopsWPDemo\BlockEditor
+ */
+class BlockRegistrar {
+
+}
