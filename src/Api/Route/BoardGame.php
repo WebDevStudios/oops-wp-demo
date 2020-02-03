e4519478d0223f375471904f595947229eb483df
Jeremy Ward <jeremy@jmichaelward.com>
2020-02-03 17:59:35 -0600

    Additional work-in-progress route handling.

--- src/Api/Route/BoardGame.php
+++ src/Api/Route/BoardGame.php
@@ -1,6 +1,14 @@
 <?php
 /**
+ * An example custom API route.
  *
+ * Astute followers of OOPS-WP will notice that there's not yet an abstraction for this class. Don't worry, it's coming
+ * eventually.
+ *
+ * For now, I wanted to register a custom endpoint that I could ping to leverage the transients we established in the
+ * Shortcode example for the EditorBlock one. That is, if a game already exists in our transient cache, let's return
+ * its data so that the Game editor block can render itself in the editor. If not, we'll hit up the BoardGameGeek API
+ * to see whether a given ID is a valid one.
  *
  * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
  * @since   2020-02-03
@@ -9,6 +17,9 @@
 
 namespace WebDevStudios\OopsWPDemo\Api\Route;
 
+use WebDevStudios\OopsWP\Utility\Registerable;
+use WebDevStudios\OopsWPDemo\Content\Shortcode\NewestGameData;
+
 /**
  * Class BoardGame
  *
@@ -16,6 +27,67 @@ namespace WebDevStudios\OopsWPDemo\Api\Route;
  * @since   2020-02-03
  * @package WebDevStudios\OopsWPDemo\Api\Route
  */
-class BoardGame {
+class BoardGame extends \WP_REST_Controller implements Registerable {
+	/**
+	 * The route namespace.
+	 *
+	 * @var string
+	 * @since 2020-02-03
+	 */
+	protected $namespace = 'oops-wp-demo/v1';
 
+	/**
+	 * The route base name.
+	 *
+	 * @var string
+	 * @since 2020-02-03
+	 */
+	protected $rest_base = 'boardgame';
+
+	/**
+	 * Register custom rest routes to work with game data.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	public function register() {
+		$this->register_routes();
+
+	}
+
+	/**
+	 * Register custom routes.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			"/{$this->rest_base}/?P<id>\d+)",
+			[
+				'methods'  => \WP_REST_Server::READABLE,
+				'callback' => [ $this, 'get_item' ],
+			]
+		);
+	}
+
+	/**
+	 * Get data for a given board game.
+	 *
+	 * @param \WP_REST_Request $request A request instance.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 * @return void|\WP_Error|\WP_REST_Response
+	 */
+	public function get_item( $request ) {
+		$game_id = $request->get_param( 'id' );
+		$data    = get_transient( NewestGameData::CACHE_ID_PREFIX . $game_id );
+
+		if ( $data ) {
+			return $data;
+		}
+	}
 }
+
--- src/Api/RouteRegistrar.php
+++ src/Api/RouteRegistrar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * An example registrar for custom API routes.
  *
  * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
  * @since   2020-02-03
@@ -9,6 +9,10 @@
 
 namespace WebDevStudios\OopsWPDemo\Api;
 
+use WebDevStudios\OopsWP\Structure\Service;
+use WebDevStudios\OopsWP\Utility\Registerable;
+use WebDevStudios\OopsWPDemo\Api\Route\BoardGame;
+
 /**
  * Class RouteRegistrar
  *
@@ -16,6 +20,54 @@ namespace WebDevStudios\OopsWPDemo\Api;
  * @since   2020-02-03
  * @package WebDevStudios\OopsWPDemo\Api
  */
-class RouteRegistrar {
+class RouteRegistrar extends Service {
+	/**
+	 * Custom example routes.
+	 *
+	 * @var array
+	 * @since 2020-02-03
+	 */
+	private $routes = [
+		BoardGame::class,
+	];
 
+	/**
+	 * Register this service's custom REST routes with WordPress.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	public function register_hooks() {
+		add_action( 'rest_api_init', [ $this, 'register_routes' ] );
+	}
+
+	/**
+	 * Register our custom routes.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	public function register_routes() {
+		foreach ( $this->routes as $route_class ) {
+			$this->register_route( new $route_class() );
+		}
+	}
+
+	/**
+	 * Register an API route with WordPress.
+	 *
+	 * @TODO Followers of OOPS-WP will recognize that we're type-hinting against an interface specific to REST routes
+	 *       in this case. Rather, we're going for the more generic Registerable. The reason here is two-fold: 1. As of
+	 *       version 0.3.0, there is not yet an abstraction for registering custom REST routes in OOPS-WP. 2. We still
+	 *       want to type-hint against _some_ interface, and once there is an abstraction, you can _rest_ assured
+	 *       that it will extend Registerable. Terribly sorry for the bad pun.
+	 *
+	 * @param Registerable $route A registerable object.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	private function register_route( Registerable $route ) {
+		$route->register();
+	}
 }
